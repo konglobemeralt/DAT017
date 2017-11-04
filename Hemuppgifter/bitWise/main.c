2926c2ef9a7ca05c75b3fcfa32f0c3b5c735b670
Konglobemeralt <jesper.blidkvist@live.se>
2017-11-04 12:08:27 +0100

    Added base for animated bit pattern

--- Hemuppgifter/bitWise/main.c
+++ Hemuppgifter/bitWise/main.c
@@ -1,11 +1,28 @@
 #include <stdio.h>
 
+void convertShortToByte(short byte){
+    
+    for(int i=15; i>=0; i--) {
+        int bit = (byte >> i) & 0x01;
+        printf("%d", bit);
+        }
+    }
+
 int main(int argc, char **argv)
 {
-	printf("hello world\n");
+	system("clear");
+    short var = 0x8000; 
     
-    
-    
-    
+    do{
+        convertShortToByte(var);
+        short tempBitmask = ~var;
+        var = var >> 1;
+        var &= tempBitmask; 
+        getchar();
+    }
+    while(1);
+        
 	return 0;
 }
+
+
