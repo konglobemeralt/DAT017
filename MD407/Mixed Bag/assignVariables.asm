1351abba82d280b9565e3281249aa97b512743b8
Konglobemeralt <jesper.blidkvist@live.se>
2017-11-18 13:19:37 +0100

    Added more excercises

--- MD407/Mixed Bag/assignVariables.asm
+++ MD407/Mixed Bag/assignVariables.asm
@@ -12,3 +12,31 @@ s:	.space	2
 	.align	2	@Allowed divisible by two	
 i:	.space	4
 	.align	4	@Allowed divisible by four
+
+@
+@	static unsigned char cu,
+@	static unsigned short su,
+@	static unsigned int iu,
+@
+ 	.global cu, su, iu
+cu:	.space	1
+	.align	1	@Allowed anywhere
+su:	.space	2
+	.align	2	@Allowed divisible by two	
+iu:	.space	4
+	.align	4	@Allowed divisible by four
+
+
+@	unsigned char	c = 'A'; 
+@	unsigned short	s = 0x1234; 
+@	unsigned int	i = 0x89ABCDEF;
+
+	.global cb, sb, ib
+cb:	.BYTE	'A'
+	.align	1
+
+sb:	.HWORD	0x1234
+	.ALIGN	2
+
+ib:	.WORD	0x89ABCDEF
+	.align	4
