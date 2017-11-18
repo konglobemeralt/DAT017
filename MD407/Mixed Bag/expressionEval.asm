6ed99d8378add43d9c581d3dd3df8e8a92117fb9
Konglobemeralt <jesper.blidkvist@live.se>
2017-11-18 13:59:50 +0100

    more expression eval

--- MD407/Mixed Bag/expressionEval.asm
+++ MD407/Mixed Bag/expressionEval.asm
@@ -28,3 +28,35 @@ d)
 	
 	ADD	R1, R1, R2
 	MUL	R0, R0, R1
+
+
+@	1.3.5. Utgå från att följande deklarationer är gjorda på global nivå. 
+@	int a,b,c; 
+@	Visa en kodsekvens, i ARM assemblerspråk, som evaluerar följande uttrycks värde till register R0. 
+@	a)	a & 4; 
+@	b)	a & b; 
+@	c)	~(a&8); 
+@	d)	~a&8;
+
+a)	
+	LDR 	R1, a
+	MOV 	R2, #4
+	AND	R0, R1, R2
+
+b)
+	LDR	R1, a
+	LDR	R2, b
+	AND	R0, R1, R2
+
+c)	
+	LDR	R1, a
+	MOV	R2, #8
+	AND	R0, R1, R2
+	MVN	R0, R0
+
+d)	
+	LDR	R1, a
+	MVN	R1, R1
+	MOV	R2, #8
+	AND	R0, R1, R2
+		
