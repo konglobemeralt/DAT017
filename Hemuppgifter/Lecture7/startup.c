35e1343bbec449bca079ed7f008b300b6d5e8ae2
Konglobemeralt <jesper.blidkvist@live.se>
2017-12-28 23:46:39 +0100

    port defs

--- Hemuppgifter/Lecture7/startup.c
+++ Hemuppgifter/Lecture7/startup.c
@@ -2,11 +2,19 @@
  * 	startup.c
  *
  */
- 
  #define GPIO_D 0x40020C00
+ #define GPIO_E 0x40021000
  
- #define GPIO_D_MODER ((volatile unsigned int *) (GPIO_D))
- #define GPIO_D_OTYPER ((volatile unsigned short *) (GPIO_D) + 0x4)
+ #define GPIO_D_MODER ((volatile unsigned int*) (GPIO_D));
+ #define GPIO_D_OTYPER ((volatile unsigned short*) (GPIO_D + 0x4)); //4
+ #define GPIO_D_PUPDR ((volatile unsigned int*) (GPIO_D + 0xC)); //12
+ #define GPIO_D_LOW_HIGH ((volatile unsigned int*) (GPIO_D + 0x11)); //12
+ #define GPIO_D_LOW_IDR ((volatile unsigned int*) (GPIO_D + 0x14)); 
+ 
+ #define GPIO_E_MODER ((volatile unsigned int *) (GPIO_E));
+ #define GPIO_E_OTYPER ((volatile unsigned short *) (GPIO_E) + 0x4);
+ #define GPIO_E_PUPDR ((volatile unsigned int *) (GPIO_E) + 0xC);
+ #define GPIO_E_LOW_IDR ((volatile unsigned char *) (GPIO_E) + 0x10);
  
 void startup(void) __attribute__((naked)) __attribute__((section (".start_section")) );
 
@@ -28,5 +36,7 @@ void init_app(){
 
 void main(void)
 {
+    
+    
 }
 
